559f92e3b07a106140d7d57cb7bccf977e502084
HaoPham1412 <phamanhhao1412@gmail.com>
2023-05-23 00:38:44 +0700

    commit before pull

--- Project/Controllers/HomeController.cs
+++ Project/Controllers/HomeController.cs
@@ -11,14 +11,6 @@ namespace Project.Controllers
     {
         AppleDataDataContext db = new AppleDataDataContext();
 
-        private int CountProductIDs(int? makh)
-        {
-            int count = 0;
-            var gioHangList = db.GioHangs;
-            var idList = gioHangList.Where(g => g.makh == makh).Select(g => g.masp).ToList();
-            count = idList.Distinct().Count();
-            return count;
-        }
         private decimal Total(int makh)
         {
             decimal total = 0;
@@ -29,16 +21,6 @@ namespace Project.Controllers
             }
             return total;
         }
-        public decimal Sale(int masp)
-        {
-            decimal sale = 0;
-            var item = db.Items.FirstOrDefault(i => i.ma == masp);
-            if (item != null)
-            {
-                sale = (decimal)(item.giaban - (item.giaban * 0.09m));
-            }
-            return sale;
-        }
         public ActionResult Index()
         {
             ViewBag.iPhone = db.Items.Where(s => s.maloai == 1).Take(4).ToList();
@@ -48,8 +30,6 @@ namespace Project.Controllers
         }
         public ActionResult Store()
         {
-            int ma = 14;
-            ViewBag.Sale = Sale(ma);
             ViewBag.iPhone = db.Items.Where(s => s.maloai == 1).Take(4).ToList();
             ViewBag.mac = db.Items.Where(s => s.maloai == 2).Take(4).ToList();
             ViewBag.iPad = db.Items.Where(s => s.maloai == 3).Take(4).ToList();
@@ -60,18 +40,35 @@ namespace Project.Controllers
         [HttpPost]
         public JsonResult Search(string searchTerm)
         {
-            // Thực hiện tìm kiếm trong cơ sở dữ liệu dựa trên searchTerm
-            var results = db.Items.Where(item => item.ten.Contains(searchTerm)).Select(item => new
-            {
-                id = item.ma,
-                name = item.ten,
-                hinh = item.hinh,
-                giaban = item.giaban,
-                giamgia = item.giamgia
-            }).ToList().Take(4);
+            // Loại bỏ khoảng trắng ở đầu và cuối chuỗi tìm kiếm
+            searchTerm = searchTerm.Trim();
+
+            // Tìm vị trí của khoảng trắng đầu tiên trong chuỗi tìm kiếm
+            int firstSpaceIndex = searchTerm.IndexOf(' ');
+
+            // Lấy từ khóa đầu tiên từ đầu chuỗi tới vị trí khoảng trắng đầu tiên (nếu có)
+            string firstKeyword = firstSpaceIndex >= 0 ? searchTerm.Substring(0, firstSpaceIndex) : searchTerm;
+
+            // Thực hiện tìm kiếm trong cơ sở dữ liệu dựa trên từ khóa đầu tiên
+            var results = db.Items
+                .Where(item => item.ten.Contains(firstKeyword))
+                .Select(item => new
+                {
+                    id = item.ma,
+                    name = item.ten,
+                    hinh = item.hinh,
+                    giaban = item.giaban,
+                    giamgia = item.giamgia
+                })
+                .ToList()
+                .Take(4);
 
             return Json(results, JsonRequestBehavior.AllowGet);
         }
+
+
+
+
         public ActionResult Product_Details(int id)
         {
             KhachHang kh = (KhachHang)Session["TaiKhoan"];
@@ -81,7 +78,6 @@ namespace Project.Controllers
             }
             var item = db.Items.FirstOrDefault(d => d.ma == id);
             ViewBag.HinhAnh = db.HinhAnhs.Where(s => s.ma == id).ToList();
-            ViewBag.CountCart = CountProductIDs(kh?.makh);
             var comments = (from d in db.DanhGias
                             join k in db.KhachHangs on d.makh equals k.makh
                             where d.masp == id && d.Trangthai == true
@@ -98,23 +94,10 @@ namespace Project.Controllers
                                 },
                             }).ToList();
             ViewBag.Comments = comments;
-
+            ViewBag.countReview = db.DanhGias.Where(i => i.masp == id).Count();
             return View(item);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
 
         [ChildActionOnly]
         public ActionResult MenuView()
@@ -122,8 +105,8 @@ namespace Project.Controllers
             KhachHang kh = (KhachHang)Session["TaiKhoan"];
             if (kh != null)
             {
-                ViewBag.CountCart = CountProductIDs(kh.makh);
                 ViewBag.TotalPrice = Total(kh.makh);
+                ViewBag.CountCart = db.GioHangs.Where(k => k.makh == kh.makh).Count();
 
             }
             var menu = db.Loais.ToList();
